494310835a8a158485cadf9237e7a60eaf0c73db
Jasper <28489757+jasval@users.noreply.github.com>
2025-05-03 00:43:10 +0100

    Fixing threading and simplifying macros

--- Sources/MVVMacroAPI/ActionHandlingViewModel.swift
+++ Sources/MVVMacroAPI/ActionHandlingViewModel.swift
@@ -4,34 +4,8 @@
 //
 //  Created by Jasper S. Valdivia on 02/05/2025.
 //
-import Combine
-
 @MainActor
-public protocol ActionHandlingViewModel: ObservableObject {
+public protocol ActionHandlingViewModel {
     associatedtype SendableAction: ActionProtocol
-    var actionHandlers: [SendableAction.Key: (Any?) -> Void] { get }
-
     func send(_ action: SendableAction)
-}
-
-@MainActor
-public extension ActionHandlingViewModel {
-    func send(_ action: SendableAction) {
-        actionHandlers[action.key]?(action.associatedValue)
-    }
-
-    func bindHandler<T>(_ method: @escaping @MainActor (T) -> Void) -> (Any?) -> Void {
-        return { @MainActor [weak self] value in
-            guard self != nil else {
-                // Debugging log: You can safely remove this in production
-                assertionFailure("ViewModel was deallocated before handler was executed")
-                return
-            }
-            guard let typedValue = value as? T else {
-                assertionFailure("Unexpected type for value: \(String(describing: value))")
-                return
-            }
-            method(typedValue)
-        }
-    }
 }
--- Sources/MVVMacroAPI/MVVMacroAPI.swift
+++ Sources/MVVMacroAPI/MVVMacroAPI.swift
@@ -4,6 +4,6 @@
 public macro Action() = #externalMacro(module: "MVVMacroImpl", type: "ActionProtocolMacro")
 
 /// A macro that produces action handling capabilities for a ViewModel
-@attached(member, names: named(SendableAction), named(actionHandlers))
+@attached(member, names: named(SendableAction), named(handle(action:)), named(send))
 @attached(extension, conformances: ActionHandlingViewModel)
 public macro ActionHandlingViewModel() = #externalMacro(module: "MVVMacroImpl", type: "ActionHandlingViewModelMacro")
